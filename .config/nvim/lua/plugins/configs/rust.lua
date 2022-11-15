b7f65fb1b3f79b757da5ce9c4cdeda47eec16124
Maksim Karelov <max.karelov@gmail.com>
2022-11-16 00:46:25 +0600

    Remove debug print

--- .config/nvim/lua/plugins/configs/rust.lua
+++ .config/nvim/lua/plugins/configs/rust.lua
@@ -1,8 +1,6 @@
 local present1, _ = pcall(require, 'lspconfig')
 local present2, rust = pcall(require, 'rust-tools')
 
-print(present1, present2)
-
 if not (present1 and present2) then
     return
 end
