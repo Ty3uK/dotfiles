69fbcabca84b978f24b5ab5e24cb0431e02d42aa
Maksim Karelov <max.karelov@gmail.com>
2023-03-21 14:54:35 +0600

    Fix lua lsp config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -44,7 +44,7 @@ capabilities.textDocument.completion.completionItem = {
     },
 }
 
-lspconfig.sumneko_lua.setup {
+lspconfig.lua_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
 
