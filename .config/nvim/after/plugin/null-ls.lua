3c85f46a4e457bc1fe69b474e9176d3c7a025dcc
Maksim Karelov <max.karelov@gmail.com>
2023-07-05 19:22:41 +0600

    Add prettier and rustfmt, fix eslint project detection

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -1,31 +1,22 @@
-local null_ls = require("null-ls");
+local null_ls = require("null-ls")
 local h = require("null-ls.helpers")
 local u = require("null-ls.utils")
 
 local eslint_d_config = {
     cwd = h.cache.by_bufnr(function(params)
-        return u.root_pattern(
+        return u.root_pattern("package.json")(params.bufname)
+    end),
+    condition = function(utils, _)
+        return utils.root_has_file(
             "eslint.config.js",
             ".eslintrc",
             ".eslintrc.js",
             ".eslintrc.cjs",
             ".eslintrc.yaml",
             ".eslintrc.yml",
-            ".eslintrc.json",
-            "package.json"
-        )(params.bufname)
-    end),
-    -- condition = function(utils)
-    --     return utils.root_has_file({
-    --         "eslint.config.js",
-    --         ".eslintrc",
-    --         ".eslintrc.js",
-    --         ".eslintrc.cjs",
-    --         ".eslintrc.yaml",
-    --         ".eslintrc.yml",
-    --         ".eslintrc.json",
-    --     })
-    -- end,
+            ".eslintrc.json"
+        )
+    end,
 }
 
 null_ls.setup({
@@ -34,7 +25,11 @@ null_ls.setup({
         null_ls.builtins.diagnostics.eslint_d.with(eslint_d_config),
         null_ls.builtins.code_actions.eslint_d.with(eslint_d_config),
 
+        null_ls.builtins.formatting.prettierd,
+
         null_ls.builtins.diagnostics.luacheck,
         null_ls.builtins.formatting.stylua,
+
+        null_ls.builtins.formatting.rustfmt,
     },
 })
