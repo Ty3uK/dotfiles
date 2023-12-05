ce06e2e202680007413403bf9812f5a9b34a6358
Maksim Karelov <max.karelov@gmail.com>
2023-12-05 14:40:25 +0700

    Remove obsidian config

--- .config/nvim/after/plugin/obsidian.lua
+++
@@ -1,9 +0,0 @@
-require("obsidian").setup({
-    dir = "/Users/ty3uk/Library/Mobile Documents/iCloud~md~obsidian/Documents/Main",
-    completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-        new_notes_location = "current_dir",
-    },
-    finder = "telescope.nvim",
-})
