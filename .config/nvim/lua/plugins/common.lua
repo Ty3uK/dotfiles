5fe076b29ce4d04158bbf4806df9357b5d719841
Maksim Karelov <max.karelov@gmail.com>
2025-04-03 12:56:48 +0700

    Stash changes for future

--- .config/nvim/lua/plugins/common.lua
+++ .config/nvim/lua/plugins/common.lua
@@ -1,81 +1,33 @@
 return {
+  {
     {
-        {
-            "ThePrimeagen/harpoon",
-            branch = "harpoon2",
-            dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-            config = function()
-                local harpoon = require("harpoon")
-                harpoon:setup()
-
-                -- basic telescope configuration
-                local conf = require("telescope.config").values
-                local function toggle_telescope(harpoon_files)
-                    local file_paths = {}
-                    for _, item in ipairs(harpoon_files.items) do
-                        table.insert(file_paths, item.value)
-                    end
-
-                    require("telescope.pickers")
-                        .new({}, {
-                            prompt_title = "Harpoon",
-                            finder = require("telescope.finders").new_table({
-                                results = file_paths,
-                            }),
-                            previewer = conf.file_previewer({}),
-                            sorter = conf.generic_sorter({}),
-                            initial_mode = "normal",
-                        })
-                        :find()
-                end
-
-                vim.keymap.set("n", "<C-h>", function()
-                    harpoon:list():add()
-                end, {noremap = true})
-                vim.keymap.set("n", "<CS-P>", function()
-                    harpoon:list():prev()
-                end, {noremap = true})
-                vim.keymap.set("n", "<CS-N>", function()
-                    harpoon:list():next()
-                end, {noremap = true})
-                vim.keymap.set("n", "<C-e>", function()
-                    toggle_telescope(harpoon:list())
-                end, { desc = "Open harpoon window" })
-            end,
+      "folke/snacks.nvim",
+      opts = {
+        indent = {
+          enabled = true,
+          animate = {
+            enabled = false,
+          },
         },
-        {
-            "lukas-reineke/indent-blankline.nvim",
-            config = function()
-                require("ibl").setup({
-                    exclude = {
-                        filetypes = {
-                            "help",
-                            "terminal",
-                            "packer",
-                            "lspinfo",
-                            "TelescopePrompt",
-                            "TelescopeResults",
-                            "",
-                        },
-                        buftypes = { "terminal" },
-                    },
-                    scope = {
-                        enabled = true,
-                        show_start = true,
-                    },
-                })
-            end,
+        scope = {
+          enabled = true,
         },
-        {
-            "mbbill/undotree",
-            keys = {
-                {
-                    "<leader>u",
-                    function()
-                        vim.cmd("UndotreeToggle")
-                    end,
-                },
-            },
+        win = {
+          border = "rounded",
+          backdrop = false,
         },
+      },
     },
+    {
+      "mbbill/undotree",
+      keys = {
+        {
+          "<leader>u",
+          function()
+            vim.cmd("UndotreeToggle")
+          end,
+        },
+      },
+    },
+  },
 }
--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -3,18 +3,15 @@ vim.g.mapleader = " "
 
 -- Indenting
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = { "*.json", "*.yml", "*.yaml", "*.toml", "*.html" },
-    callback = function()
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
-    end,
+vim.filetype.add({
+    pattern = {
+        ["hypr.*"] = "hyprlang",
+    },
 })
 
 -- Numbers
@@ -37,6 +34,3 @@ vim.opt.updatetime = 250
 vim.g.markdown_fenced_languages = {
     "ts=typescript",
 }
-
-vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
-vim.g.netrw_localrmdir = "rm -rf"
