0ab8e0286bca2fd80dca18fea338c8b1716146d7
Maksim Karelov <max.karelov@gmail.com>
2026-02-22 21:00:19 +0700

    feat: tweak nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.pack.add({
     "https://github.com/nvim-lua/plenary.nvim",
     "https://github.com/ellisonleao/gruvbox.nvim",
     "https://github.com/nvim-treesitter/nvim-treesitter",
+    "https://github.com/nvim-treesitter/nvim-treesitter-context",
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/mason-org/mason.nvim",
     "https://github.com/mason-org/mason-lspconfig.nvim",
@@ -9,11 +10,12 @@ vim.pack.add({
     "https://github.com/nvimtools/none-ls.nvim",
     "https://github.com/nvimtools/none-ls-extras.nvim",
     "https://github.com/nvim-mini/mini.diff",
-    "https://github.com/nvim-mini/mini.pairs",
     "https://github.com/nvim-mini/mini.trailspace",
     "https://github.com/mikavilpas/yazi.nvim",
     "https://github.com/folke/snacks.nvim",
+    "https://github.com/grafana/vim-alloy",
 })
+vim.cmd("packadd nvim.undotree")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
@@ -31,6 +33,7 @@ require("gruvbox").setup({
     },
 })
 vim.cmd.colorscheme("gruvbox")
+vim.api.nvim_set_hl(0, "SnippetTabstopActive", { ng = nil })
 
 vim.opt.signcolumn = "yes"
 vim.g.mapleader = " "
@@ -57,29 +60,32 @@ require("blink.cmp").setup({
 })
 
 require("nvim-treesitter.configs").setup({
+    modules = {},
+    ensure_installed = {},
+    ignore_install = {},
+    sync_install = false,
     highlight = {
-        enable = true,
-    },
-    indent = {
         enable = true,
     },
     auto_install = true,
 })
+require("treesitter-context").setup({
+    enable = true,
+    max_lines = 1,
+})
 
 require("mini.diff").setup({
     view = {
-        style = "sign"
-    }
-})
-require("mini.pairs").setup({
-    mappings = {
-        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
-        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
-        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
+        style = "sign",
     },
 })
 require("mini.trailspace").setup()
-require("yazi").setup({ open_for_directories = true })
+require("yazi").setup({
+    open_for_directories = true,
+    init = function()
+        vim.g.loaded_netrwPlugin = 1
+    end,
+})
 
 require("snacks").setup({
     indent = {
@@ -100,58 +106,86 @@ require("snacks").setup({
             input = {
                 keys = {
                     ["<c-y>"] = { "confirm", mode = { "n", "i" } },
-                }
-            }
-        }
+                },
+            },
+        },
     },
     input = {
         enabled = true,
     },
+    styles = {
+        input = {
+            relative = "cursor",
+        },
+    },
 })
 
 -- Keymaps
-vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files({ hidden = true }) end,
-    { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>lg", function() Snacks.picker.grep() end,
-    { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>fb", function() require("yazi").yazi() end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>gd", function() Snacks.picker.lsp_definitions() end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>gr", function() Snacks.picker.lsp_references() end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>ra", function() vim.lsp.buf.rename() end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>gt", function() Snacks.lazygit() end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>f", function() vim.diagnostic.open_float() end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>fm", function() vim.lsp.buf.format() end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>gb", function() Snacks.git.blame_line() end, { noremap = true, silent = true })
-vim.keymap.set("n", '<Esc>', ':noh<CR><Esc>', { noremap = true, silent = true })
-vim.keymap.set("n", '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.keymap.set("n", '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ff", function()
+    Snacks.picker.files({ hidden = true })
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>lg", function()
+    Snacks.picker.grep()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fb", function()
+    require("yazi").yazi()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ca", function()
+    vim.lsp.buf.code_action()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gd", function()
+    Snacks.picker.lsp_definitions()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gr", function()
+    Snacks.picker.lsp_references()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ra", function()
+    vim.lsp.buf.rename()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gt", function()
+    Snacks.lazygit()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>f", function()
+    vim.diagnostic.open_float()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fm", function()
+    vim.lsp.buf.format()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gb", function()
+    Snacks.git.blame_line()
+end, { noremap = true, silent = true })
+vim.keymap.set("n", "<Esc>", ":noh<CR><Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
 
 -- LSP
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.sql_formatter.with({ command = { "sleek" } }),
         null_ls.builtins.diagnostics.selene,
+        null_ls.builtins.formatting.sql_formatter.with({ command = { "sleek" } }),
     },
 })
 
 vim.lsp.config("sqls", {
-    cmd = { 'sqls', '-config', 'sqls.config.yaml' },
-    root_markers = { 'sqls.config.yaml' },
+    cmd = { "sqls", "-config", "sqls.config.yaml" },
+    root_markers = { "sqls.config.yaml" },
     on_attach = function(client, _)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
-    end
+    end,
 })
 
 vim.lsp.config("lua_ls", {
     settings = {
         Lua = {
             runtime = {
-                version = 'LuaJIT',
-                path = vim.split(package.path, ';'),
+                version = "LuaJIT",
+                path = vim.split(package.path, ";"),
             },
             workspace = {
                 checkThirdParty = false,
@@ -161,7 +195,7 @@ vim.lsp.config("lua_ls", {
                 },
             },
             diagnostics = {
-                globals = { 'vim' },
+                globals = { "vim" },
             },
         },
     },
